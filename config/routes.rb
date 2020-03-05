7444ad0b469afe2dd99490192ac7abde5c58223f
Creeland <cree@provinsal.com>
2020-03-05 14:22:31 -0800

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
 
   resources :users
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
